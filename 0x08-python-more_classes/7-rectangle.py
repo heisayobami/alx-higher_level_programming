7329a547327e1c30888466ad9c2dea5bd8655f96
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-01-03 01:23:41 +0100

    task 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,17 +1,5 @@
 #!/usr/bin/python3
-"""
-This is the "Rectangle"  module.
-This module provides a Rectangle class.
-"""
-
-
 class Rectangle:
-    """A Rectangle class with attributes width and height,
-    methods area, perimeter, print, str, repr, and del, and
-    class attribute number_of_instances that keeps track of # of instances,
-    and class attribute print_symbol which is used as symbol for printing.
-    """
-
     number_of_instances = 0
     print_symbol = "#"
 
