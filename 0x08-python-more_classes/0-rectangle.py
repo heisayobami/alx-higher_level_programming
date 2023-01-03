f9b0d2eaadc8d86a8ea94c3e31dadaf94d4c15c4
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-01-03 01:40:42 +0100

    task 0

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+This is the "Rectangle"  module.
+This module provides a simple Rectangle class.
+"""
+
 
 class Rectangle:
-    """A typical Rectangle class"""
+    """A simple Rectangle class"""
     pass
-  
