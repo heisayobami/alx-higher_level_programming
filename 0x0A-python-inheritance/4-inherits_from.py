08c45be39edcd524815d3f0d8bb4dad9c40fa7d6
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-02-01 12:21:19 +0100

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@ Contains a certain inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """A function that returns True only if obj is sub-class of a_class, otherwise False"""
+    """Returns True only if obj is sub-class of a_class, otherwise False"""
     return (type(obj) != a_class and issubclass(type(obj), a_class))
