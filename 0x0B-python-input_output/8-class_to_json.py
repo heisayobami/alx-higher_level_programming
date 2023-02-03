a462b69711dff2828fbbe251ed3b00cfc8994c5f
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-02-03 23:54:27 +0100

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,10 +2,10 @@
 """ Module that returns the dictionary description with a simple
 data structure for a JSON serialization of an object
 """
-import json
 
 def class_to_json(obj):
     """ Function that returns the dictionary description of an obj """
+    
     result = {}
     if hasattr(obj, "__dict__"):
         result = obj.__dict__.copy()
