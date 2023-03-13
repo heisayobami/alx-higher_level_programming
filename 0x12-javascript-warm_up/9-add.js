37583307019dc69372dadcdb0ed0b85d12d64a45
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-03-13 21:09:48 +0100

    Update 9-add.js

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
 
-function add (a, b) {
-  return (a + b);
-}
-
-if (process.argv.length < 4) {
-  console.log('NaN');
-} else {
-  console.log(add(parseInt(process.argv[2]), parseInt(process.argv[3])));
-}
+function add (a, b) { console.log(a + b); }
+add(Number(process.argv[2]), Number(process.argv[3]));
