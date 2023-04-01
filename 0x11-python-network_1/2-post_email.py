928fc27a56900e4acaca6875cd51ca9a12949eb5
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-01 11:56:42 +0100

    Update 2-post_email.py

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL and an email, sends a POST request to the passed URL with the
-email as a parameter, and displays the body of the response (utf-8)
+Python script that takes in a URL and an email, sends a POST request to the 
+passed URL with the email as a parameter, and displays the body of the response
 """
 if __name__ == "__main__":
     import urllib.parse as parse
