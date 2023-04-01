72793cc0ceff4fd4547cf607185f299e7e501afd
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-01 11:57:05 +0100

    Update 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Python script that takes in a URL, sends a request to the URL & displays the body of the response (utf-8)
+Python script that takes in a URL, sends a request to the URL,
+and displays the body of the response (utf-8)
 """
 if __name__ == "__main__":
     import urllib.error as error
