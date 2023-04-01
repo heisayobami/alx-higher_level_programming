c93aae104d02da7879447bb4cac37adab4822d74
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-01 11:39:06 +0100

    Create 10-my_github.py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+A script that takes your Github credentials, and uses the GitHub API to dispaly your ID.
+"""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = 'https://api.github.com/user'
+    response = requests.get(url, auth=(argv[1], argv[2]))
+    try:
+        print(response.json().get("id"))
+    except ValueError:
+        print("Not a valid JSON")
