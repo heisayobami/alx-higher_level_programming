b98907c18ba05e2bacca570693a1f17685a0a64a
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-12 00:51:35 +0100

    Update 6-states.sql

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -6,5 +6,6 @@
 -- If the table states already exists, script does not fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL);
