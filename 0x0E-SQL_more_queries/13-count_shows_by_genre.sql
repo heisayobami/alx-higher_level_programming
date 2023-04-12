1a826ec897c8c9b3c5f1783e750282371d0ede4b
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-12 02:23:30 +0100

    Update 13-count_shows_by_genre.sql

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,15 +1,17 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each.
--- Each record should display: tv_genres.name - number of shows
--- Don’t display a genre that doesn’t have any shows linked
--- First column must be called genre
--- Second column must be called number_of_shows
--- Results must be sorted in descending order by the number of shows linked
--- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- Each record displays: tv_genres.name - number of shows
+-- Genres without any shows linked aren't displayed
+-- First column is called genre
+-- Second column is called number_of_shows
+-- Results sorted in descending order by the number of shows linked
+-- Only one SELECT statement is allowed
+-- The database name is passed as an argument of the mysql command
 
-SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres
-INNER JOIN tv_show_genres
-ON tv_genres.id=tv_show_genres.genre_id
-GROUP BY tv_show_genres.genre_id
+SELECT
+	tg.name AS genre,
+  COUNT(tg.id) AS number_of_shows
+FROM tv_genres tg 
+INNER JOIN tv_show_genres tsg 
+  ON tg.id = tsg.genre_id
+GROUP BY tg.id
 ORDER BY number_of_shows DESC;
