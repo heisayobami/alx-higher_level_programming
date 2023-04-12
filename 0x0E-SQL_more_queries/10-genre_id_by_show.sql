32538b76555ced56607b423c19209905fe8f082b
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-12 01:30:40 +0100

    Update 10-genre_id_by_show.sql

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,11 +1,13 @@
 --  lists all shows contained in hbtn_0d_tvshows that have at least one genre linked.
 -- Each record should display: tv_shows.title - tv_show_genres.genre_id
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- Results are sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
+-- Only one SELECT statement is allowed
+-- The database name is passed as an argument of the mysql command
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-INNER JOIN tv_show_genres
-ON tv_shows.id=tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT 
+      ts.title,
+      tsg.genre_id
+FROM tv_shows ts
+INNER JOIN tv_show_genres tsg
+      ON ts.id = tsg.show_id
+ORDER BY ts.title, tsg.genre_id;
