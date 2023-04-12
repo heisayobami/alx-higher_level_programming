1ae92e8713bcae7cfa82b79a2be4756b40c2f088
Ayobami Ayodeji <ayodejiayobami99@gmail.com>
2023-04-12 02:48:58 +0100

    Update 16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,14 +1,16 @@
 -- lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
--- If a show doesn’t have a genre, display NULL in the genre column
--- Each record should display: tv_shows.title - tv_genres.name
--- Results must be sorted in ascending order by the show title and genre name
--- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- If a show doesn’t have a genre, NULL is displayed in the genre column
+-- Each record displays: tv_shows.title - tv_genres.name
+-- Results is sorted in ascending order by the show title and genre name
+-- Only one SELECT statement is allowed
+-- The database name is passed as an argument of the mysql command
 
-SELECT tv_shows.title, tv_genres.name
-FROM tv_shows
-LEFT JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres
-ON tv_show_genres.genre_id = tv_genres.id
-ORDER BY tv_shows.title, tv_genres.name;
+SELECT
+  ts.title,
+  tg.name
+FROM tv_genres tg
+JOIN tv_show_genres tsg 
+  ON tg.id = tsg.genre_id
+RIGHT JOIN tv_shows ts
+  ON tsg.show_id = ts.id
+ORDER BY ts.title, tg.name;
